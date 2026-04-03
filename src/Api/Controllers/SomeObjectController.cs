6d34b52d744e059e3649252dbeb2ad7708a3e292
Nikolay Kulev <n.kulev.93.work@gmail.com>
2026-04-03 21:15:10 +0300

    add controller with mock for dev environment testing, clear unused configurations on application level

---
+++ src/Api/Controllers/SomeObjectController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    public class SomeObject
+    {
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SomeObjectController : ControllerBase
+    {
+        [HttpGet]
+        public SomeObject Get([FromRoute] int id)
+        {
+            var mock = new SomeObject() { Id = id, Name = "Dummy Object Name" };
+
+            return mock;
+        }
+    }
+}
