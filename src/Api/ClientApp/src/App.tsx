c1ef26aa7bd9854bb9a5c5149ea997bd47904132
Nikolay Kulev <n.kulev.93.work@gmail.com>
2026-04-08 14:19:12 +0300

    finish initial tsconfig and package configuration files

---
+++ src/Api/ClientApp/src/App.tsx
@@ -0,0 +1,6 @@
+﻿import React from 'react';
+export const App = () => {
+    return (
+        <React.Fragment>TODO: Add 'get' request example.</React.Fragment>
+    )
+}
---
+++ src/Api/ClientApp/src/index.tsx
@@ -0,0 +1,13 @@
+﻿import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './App.jsx';
+
+const domNode = document.getElementById('root');
+if (!domNode) {
+    throw new Error("Missing root node!")
+}
+
+const root = createRoot(domNode);
+root.render(
+    <StrictMode><App />
+        </StrictMode>        );
