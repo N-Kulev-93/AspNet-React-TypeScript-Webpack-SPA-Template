bdc40d45620ddb5ae209221621f8c18648c86903
N-Kulev-93 <n.kulev.93.work@gmail.com>
2026-04-10 15:18:04 +0300

    configure client app dev server with aspnet project build for development

--- src/Api/ClientApp/webpack.config.ts
+++ src/Api/ClientApp/webpack.config.ts
@@ -14,6 +14,7 @@ const config: webpack.Configuration = {
     devServer: {
         hot: true,
         open: true,
+        port: 8095,
         proxy: [
             {
                 context: ['/api'],
