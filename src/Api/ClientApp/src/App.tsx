97c4b2c664bb3863490831686f23166253e4320b
N-Kulev-93 <n.kulev.93.work@gmail.com>
2026-04-21 18:39:39 +0300

    fix client application directory structure, add main css style import

--- src/Api/ClientApp/src/App.tsx
+++ src/Api/ClientApp/src/App.tsx
@@ -1,7 +1,24 @@
-﻿const App = () => {
+﻿import { useState } from 'react';
+import '.'
+
+const App = () => {
+    const [number, setNumber] = useState<number>();
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        const id = Number(event.currentTarget.value);
+        setNumber(id);
+    }
+    const handleClick = (): Promise<void> => {
+        return Promise.resolve();
+    }
+
     return (
-        <div>TODO: Add 'get' request example1234.</div>
-    )
+        <div className="centered-container">
+            <h2>Welcome to fat cats family fetching service! </h2> {/* TODO: complete template http action example... */}
+            <input type="number" value={number} onChange={handleChange} />
+            <button onClick={handleClick}>Fetch</button>
+        </div>
+    );
 }
 
 export default App;
--- src/Api/ClientApp/src/index.tsx
+++ src/Api/ClientApp/src/index.tsx
@@ -1,6 +1,7 @@
 ﻿import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import './App.css';
 
 const domNode = document.getElementById('root');
 if (!domNode) {
@@ -11,4 +12,4 @@ const root = createRoot(domNode);
 root.render(
     <StrictMode>
         <App />
-        </StrictMode>        );
+        </StrictMode>);
